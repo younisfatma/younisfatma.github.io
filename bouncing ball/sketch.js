c3f5534a25d4c95703d0bb16cdb9e3b42c51a528
younisfatma <58179997+younisfatma@users.noreply.github.com>
2021-01-19 13:37:19 -0600

    Bouncing ball demo

---
+++ bouncing ball/sketch.js
@@ -0,0 +1,53 @@
+// Bouncung balls
+// Array demo
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let theBouncingBalls =[];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  moveBall();
+  displayBall();
+}
+
+function mousePressed(){
+  let ball = {
+    x: mouseX,
+    y: mouseY,
+    diameter: random(25, 100),
+    dx: random(5,.5),
+    dy: random(5,.5),
+    theColor: color(random(255), random(255), random(255), random(255)),
+ };
+
+ theBouncingBalls.push(ball);
+}
+
+function moveBall(){
+  for (let ball of theBouncingBalls){
+    ball.x+=ball.dx;
+    ball.y+= ball.dy;
+
+    if (ball.x + ball.diameter >= width || ball.x - ball.diameter/3 <=0){
+    ball.dx *=-1;
+    }
+    if (ball.y + ball.diameter >= height || ball.y - ball.diameter/3 <=0){
+      ball.dy *=-1;
+    }
+  }
+}
+
+function displayBall(){
+  for (let ball of theBouncingBalls){
+    noStroke();
+    fill(ball.theColor);
+    ellipse(ball.x, ball.y, ball.diameter, ball.diameter);
+  }
+}
---
+++ cs30-p5js-template-master/cs30-p5js-template-master/sketch.js
@@ -0,0 +1,37 @@
+// Bouncung balls
+// Array demo
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let ball = [{
+  x: 50,
+  y: 200,
+  diameter: 60,
+  dx: 5,
+  dy: -3,
+}];
+
+function setup() {
+ createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+ background(220);
+ moveBall();
+ displayBall();
+}
+
+function moveBall(){
+ if (ball.x + ball.diameter >= width || ball.x - ball.diameter/3 <=0){
+ ball.dx *=-1;
+ }
+ if (ball.y + ball.diameter >= height || ball.y - ball.diameter/3 <=0){
+   ball.dy *=-1;
+   }
+}
+
+function displayBall(){
+ elipse(ball.x, ball.y, ball.diameter, ball.diameter)
+}
