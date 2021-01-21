e0bb4e54db5d75831ad3ae2aaf4a1d12decb858f
younisfatma <58179997+younisfatma@users.noreply.github.com>
2021-01-21 14:04:24 -0600

    node js

--- terrain/cs30-p5js-template-master/sketch.js
+++ terrain/cs30-p5js-template-master/sketch.js
@@ -4,27 +4,27 @@ let rectHeights;
 let bikeLocation = 0;
 
 function setup() {
- createCanvas(windowWidth, windowHeight);
- rectHeights = generateHeight();
- console.log(rectHeights);
+  createCanvas(windowWidth, windowHeight);
+  rectHeights = generateHeight();
+  console.log(rectHeights);
 }
 
 function draw() {
- background(220);
+  background(220);
 
- let howMany = width;
- for (let i = bikeLocation; i < bikeLocation + howMany; i++){
-   let rectWidth = width/howMany;
-   rect(rectWidth*(i-bikeLocation), height-rectHeights[i], rectWidth, rectHeights[i])
- }
- if (keyIsPressed){
-  if (key == "d"){
-    bikeLocation +=5
-  }  
-  if (key == "a" && bikeLocation > 0){
-    bikeLocation -=5
+  let howMany = width;
+  for (let i = bikeLocation; i < bikeLocation + howMany; i++){
+    let rectWidth = width/howMany;
+    rect(rectWidth*(i-bikeLocation), height-rectHeights[i], rectWidth, rectHeights[i]);
   }
- }
+  if (keyIsPressed){
+    if (key === "d"){
+      bikeLocation +=5;
+    }  
+    if (key === "a" && bikeLocation > 0){
+      bikeLocation -=5;
+    }
+  }
 }
 
 function generateHeight(){
