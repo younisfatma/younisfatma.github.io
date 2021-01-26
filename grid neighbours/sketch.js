f497afcb59d85a5b7d29c9caca285f69b8b5a3b1
younisfatma <58179997+younisfatma@users.noreply.github.com>
2021-01-26 13:19:41 -0600

    grid neighbour

--- grid neighbours/sketch.js
+++ grid neighbours/sketch.js
@@ -1,9 +1,21 @@
 // Grid neighbours
 
-let grid = createEmptyGrid(10,10);
+let grid = createEmptyGrid(4,4);
+let rows, cols, cellWidth, cellHeight;
+let bgmusic;
+
+function preload(){
+  bgMusic = loadSound("assets/background.mp3")
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  bgMusic.loop();
+
+  rows = grid.length;
+  cols = grid[0].length;
+  cellWidth = width/cols;
+  cellHeight = height/rows;
 }
 
 function draw() {
@@ -11,20 +23,43 @@ function draw() {
   displayGrid();
 }
 
-function displayGrid(){
-  let rows = grid.length;
-  let cols = grid[0].length;
-  let cellWidth = width/cols;
-  let cellHeight = height/rows;
+function mousePressed(){
+clickSound.play();
 
+  let x = Math.floor(mouseX/cellWidth);
+  let y = Math.floor(mouseY/cellHeight);
+
+  toggleCells(x,y); //self
+  toggleCells(x,y-1); //north
+  toggleCells(x,y+1); //south
+  toggleCells(x+1,y); //east
+  toggleCells(x-1,y); //west
+
+}
+
+function toggleCells(x,y){
+  //check that coordinates are in the array
+  //an edge case
+  if(x>= 0 && x<cols &&y >= 0 && y < rows){
+    if (grid[y][x] === 1){
+      grid[y][x] = 0;
+    }
+    else if (grid[y][x] === 0){
+      grid[y][x] = 1;
+    }
+  }
+}
+
+function displayGrid(){
   for (let y = 0; y < rows; y++){
     for (let x = 0; x < cols; x++){
       if (grid[y][x] === 0){
-        fill("blue");
+        fill("cyan");
       }
-      if (grid[y][x] = 1){
+      if (grid[y][x] === 1){
         fill("pink");
       }
+      noStroke();
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
@@ -33,7 +68,7 @@ function displayGrid(){
 function createEmptyGrid(cols, rows){
   let emptyGrid = [];
   for (let y = 0; y < rows; y++){
-    EmptyGrid.push([])
+    emptyGrid.push([])
     for (let x = 0; x < cols; x++){
       emptyGrid[y].push(0);
     }
