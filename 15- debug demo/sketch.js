60207745433f0de5c3e337552f07d53c84f74317
younisfatma <58179997+younisfatma@users.noreply.github.com>
2021-02-11 14:00:08 -0600

    restart button/starts & win & lose screen/ time/ other minor improvements

---
+++ 15- debug demo/sketch.js
@@ -0,0 +1,18 @@
+// tittle
+// date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let x, y;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  x= width/2;
+  y = height/2;
+}
+
+function draw() {
+  background(220);
+  rect(x, y, mouseX, mouseY);
+}
